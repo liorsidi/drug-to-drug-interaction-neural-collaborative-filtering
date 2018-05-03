161a28b71854da09647549037441bbe263ada315
Dvir <dvir.cohn@gmail.com>
2018-05-03 16:40:03 +0300

    added full train test split

---
+++ basic_svd.py
@@ -0,0 +1,17 @@
+import graphlab
+from graphlab.toolkits.recommender.factorization_recommender import FactorizationRecommender
+import pandas as pd
+
+
+train = graphlab.SFrame.read_csv("train_train.csv")
+test = graphlab.SFrame.read_csv("train_test.csv")
+model = graphlab.recommender.ranking_factorization_recommender.create(train, user_id='node1', item_id='node2')
+a = model.predict(test)
+
+df = pd.read_csv('train_test.csv')
+df['score'] = list(a)
+df = df.sort_values(by=['score'], ascending=False)
+
+
+
+pass
--- data_reader.py
+++ data_reader.py
@@ -1,7 +1,11 @@
 import random
-
 import networkx as nx
+import pandas as pd
+from scipy.sparse import csr_matrix
+import numpy as np
+from sklearn.model_selection import train_test_split
 
+DATA_SIZE = 1434
 path = "train.csv"
 
 
@@ -50,4 +54,55 @@ def get_nx_graph():
     graph = nx.read_edgelist(path, delimiter=',', nodetype=str)
     print "number of nodes: {}".format(graph.number_of_nodes())
     print "number of ednges: {}".format(graph.number_of_edges())
+    non_edges = nx.non_edges(graph)
+    df = pd.DataFrame(list(non_edges))
+    df['label'] = 0
+    df = df.rename(columns={0: 'node1', 1: 'node2'})
+    df2 = pd.read_csv(path)
+    df2['label'] = 1
+    full_df = df2.append(df)
+    full_df.to_csv('full_train.csv', index=False)
+
     return graph
+
+def get_non_edges():
+    pass
+
+def get_train_test_as_edge_list():
+    train, test = get_train_test()
+    nx.write_edgelist(train, 'train_train.csv')
+    nx.write_edgelist(test, 'train_test.csv')
+
+def read_sparse_matrix(train_data):
+    print 'creating matrix'
+    x = train_data['node1']
+    y = train_data['node2']
+    assert len(x) == len(y)
+    data = [1] * len(x)
+    m = csr_matrix((data,(x,y)), shape=(DATA_SIZE,DATA_SIZE),dtype='f')
+    print 'm shape:', m.shape, 'm non zeros:', m.nnz
+    assert np.allclose(m.todense(), m.T.todense(), atol=1e-8) #matrix is symmetric
+    return m.todense()#the matrix is small, sparse matrix is not necessary.
+
+def create_holdout_set(train_path,train_percent = 0.9):
+    df = pd.read_csv(train_path)
+    m_train = read_sparse_matrix(df)
+    #create holdout set. the set will contains both existing and non-existing edges.
+    m_train_holdout = np.matrix(m_train)
+    validation_set = set()
+    for i in range(DATA_SIZE):
+        for j in range(i+1, DATA_SIZE):
+            if random.random() > train_percent:
+                validation_set.add((i, j))
+                m_train_holdout[i, j] = 0
+                m_train_holdout[j, i] = 0
+    return m_train_holdout, validation_set
+
+
+def get_full_train_test():
+    df = pd.read_csv('full_train.csv')
+    X_train, X_test = train_test_split(df, test_size=0.33, random_state=42)
+    X_train.to_csv('train_train.csv', index=False)
+    X_test.to_csv('train_test.csv', index=False)
+# create_holdout_set(path)
+get_full_train_test()
